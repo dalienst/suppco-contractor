79e82780cdfc6010c824263042c088f988ca8f21
dalienst <oduordalienst@gmail.com>
2024-02-01 17:09:22 +0300

    add dummy data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PageBanner from "../components/pages/PageBanner";
 import { Link } from "react-router-dom";
+import { dummyConstructionData } from "../data/OrdersData";
 
 function Dashboard() {
   return (
@@ -56,10 +57,26 @@ function Dashboard() {
 
         <section className="mb-3 py-3">
           <div className="d-flex flex-md-row flex-column justify-content-between">
-            <div>
+            <div className="mb-2">
               <h3 className="fw-bold me-2">Orders</h3>
             </div>
-            <div>
+
+            {/* creating filters for the table */}
+            <div className="mb-3 d-flex">
+              <button className="btn mx-1 btn-sm btn-success">
+                <i className="bi bi-hourglass-bottom fst-normal">Completed</i>
+              </button>
+              <button className="btn mx-1 btn-sm btn-warning">
+                <i className="bi bi-hourglass-split fst-normal">Active</i>
+              </button>
+              <button className="btn mx-1 btn-sm btn-danger">
+                <i className="bi bi-hourglass-top fst-normal">Pending</i>
+              </button>
+            </div>
+            {/* end */}
+
+            {/* Search function */}
+            <div className="mb-2">
               <form role="search" className="d-flex">
                 <input
                   type="search"
@@ -83,8 +100,47 @@ function Dashboard() {
                   <th>Supplier Name</th>
                   <th>Supplier Company</th>
                   <th>Date</th>
+                  <th>Status</th>
                 </tr>
               </thead>
+              <tbody>
+                {dummyConstructionData.map((item) => (
+                  <>
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.product}</td>
+                      <td>{item.supplierName}</td>
+                      <td>{item.supplierCompany}</td>
+                      <td>{item.date}</td>
+                      <td>
+                        {item.status === "Pending" ? (
+                          <>
+                            <button className="btn btn-sm btn-danger w-100">
+                              <i className="bi bi-hourglass-top fst-normal">
+                                Pending
+                              </i>
+                            </button>
+                          </>
+                        ) : item.status === "Active" ? (
+                          <>
+                            <button className="btn btn-sm btn-warning w-100">
+                              <i className="bi bi-hourglass-split fst-normal">
+                                Active
+                              </i>
+                            </button>
+                          </>
+                        ) : (
+                          <button className="btn btn-sm btn-success w-100">
+                            <i className="bi bi-hourglass-bottom fst-normal">
+                              Completed
+                            </i>
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
             </table>
           </div>
         </section>
