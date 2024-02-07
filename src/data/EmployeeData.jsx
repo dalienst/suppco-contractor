8bee0fed9a3569496db783e6802daf33f3e5b24f
dalienst <oduordalienst@gmail.com>
2024-02-07 09:40:11 +0300

    add data

---
+++ src/data/EmployeeData.jsx
@@ -0,0 +1,46 @@
+const employees = [
+  {
+    id: 2,
+    name: "Jennifer",
+    position: "Manager",
+    phone: "+254 712345679",
+    email: "jMn8R@example.com",
+  },
+  {
+    id: 3,
+    name: "Michael",
+    position: "Developer",
+    phone: "+254 712345680",
+    email: "mDv9R@example.com",
+  },
+  {
+    id: 4,
+    name: "Emily",
+    position: "Designer",
+    phone: "+254 712345681",
+    email: "eDs1R@example.com",
+  },
+  {
+    id: 5,
+    name: "Daniel",
+    position: "Analyst",
+    phone: "+254 712345682",
+    email: "dAl2R@example.com",
+  },
+  {
+    id: 6,
+    name: "Jessica",
+    position: "Engineer",
+    phone: "+254 712345683",
+    email: "jEn3R@example.com",
+  },
+  {
+    id: 7,
+    name: "Ryan",
+    position: "Technician",
+    phone: "+254 712345684",
+    email: "rTc4R@example.com",
+  },
+];
+
+export {employees}
--- src/pages/Sites.jsx
+++ src/pages/Sites.jsx
@@ -1,12 +1,45 @@
-import React from "react";
-import PageBanner from "../components/pages/PageBanner";
+import React, { useState } from "react";
 import SitesDisplay from "../layouts/sites/SitesDisplay";
+import { Link } from "react-router-dom";
+import { publicLinks } from "../constants/links";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function Sites() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className="container py-3">
-        <PageBanner name="Sites" />
+        <section>
+          <div className="d-flex align-content-center justify-content-between py-2 mb-3 border px-2 rounded bg-primary-subtle  shadow">
+            <h2 className="fw-bold ">
+              <span>
+                <Link
+                  className="btn btn-primary me-2"
+                  to={publicLinks?.Dashboard}
+                >
+                  <i className="bi bi-box-arrow-left"></i>
+                </Link>
+              </span>
+              Sites
+            </h2>
+            <div>
+              <Button onClick={handleShow} className="btn btn-primary">
+                New SIte
+              </Button>
+            </div>
+
+            <Modal show={show} onHide={handleClose}>
+              <div className="modal-header">
+                <h5 className="modal-title">Add New Site</h5>
+              </div>
+            </Modal>
+          </div>
+        </section>
+
         <SitesDisplay />
       </div>
     </>
