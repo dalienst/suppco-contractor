23478caa145e533d033ab4bf1fdf49b0fee6f4ab
dalienst <oduordalienst@gmail.com>
2024-02-07 17:04:41 +0300

    display shell equipment

--- src/data/inventory/shellEquipment.jsx
+++ src/data/inventory/shellEquipment.jsx
@@ -1,5 +1,146 @@
 const shell = [
+  {
+    id: 1,
+    category: "Aggregate",
+    subCategory: [
+      {
+        id: 1,
+        name: "Sand",
+        items: [
+          {
+            id: 1,
+            item: "River Sand",
+            location: [
+              {
+                id: 1,
+                site: {
+                  name: "Site B",
+                  location: "Location B",
+                },
+                density: "12344",
+                moistureContent: "12344",
+                gradiation: "12344",
+                size: "12344",
+                shape: "12344",
+                image: "",
+                variation: "",
+                woodSpecies: "",
+                grade: "",
+                treatment: "",
+                surfaceFinish: "",
+                other: "12344",
+                supplierInfo: {
+                  name: "Big Suppliers",
+                  username: "bigsuppliers",
+                  email: "bigsuppliers@me.com",
+                  phone: "+1 123 456 7890",
+                },
+              },
+              {
+                id: 2,
+                site: {
+                  name: "Site E",
+                  location: "Location E",
+                },
+                density: "12344",
+                moistureContent: "12344",
+                gradiation: "",
+                size: "12344",
+                shape: "12344",
+                image: "",
+                variation: "",
+                woodSpecies: "",
+                grade: "",
+                treatment: "",
+                surfaceFinish: "",
+                other: "",
+                supplierInfo: {
+                  name: "Big Suppliers",
+                  username: "bigsuppliers",
+                  email: "bigsuppliers@me.com",
+                  phone: "+1 123 456 7890",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    category: "Roof",
+    subCategory: [
+      {
+        id: 1,
+        name: "Roof Truss",
+        items: [
+          {
+            id: 1,
+            item: "Lumber truss - kin dried",
+            location: [
+              {
+                id: 1,
+                site: {
+                  name: "Site B",
+                  location: "Location B",
+                },
+                density: null,
+                moistureContent: null,
+                gradiation: null,
+                size: "12344",
+                shape: "12344",
+                image: "",
+                variation: "1234",
+                woodSpecies: "Mahogany",
+                grade: "A",
+                treatment: "1234",
+                surfaceFinish: "smooth",
+                other: "12344",
+                supplierInfo: {
+                  name: "Big Suppliers",
+                  username: "bigsuppliers",
+                  email: "bigsuppliers@me.com",
+                  phone: "+1 123 456 7890",
+                },
+              },
+            ],
+          },
+          {
+            id: 2,
+            item: "Structural Graded lumber",
+            location: [
+              {
+                id: 1,
+                site: {
+                  name: "Site B",
+                  location: "Location B",
+                },
+                density: null,
+                moistureContent: null,
+                gradiation: null,
+                size: "12344",
+                shape: "12344",
+                image: "",
+                variation: "1234",
+                woodSpecies: "Mahogany",
+                grade: "A",
+                treatment: "1234",
+                surfaceFinish: "smooth",
+                other: "12344",
+                supplierInfo: {
+                  name: "Big Suppliers",
+                  username: "bigsuppliers",
+                  email: "bigsuppliers@me.com",
+                  phone: "+1 123 456 7890",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
 
-]
-
-export { shell }
+export { shell };
--- src/pages/SiteEquipment.jsx
+++ src/pages/SiteEquipment.jsx
@@ -5,14 +5,17 @@ import { publicLinks } from "../constants/links";
 function SiteEquipment() {
   return (
     <div className="container">
-      <h2 className="fw-bold ">
-        <span>
-          <Link className="btn btn-primary me-2" to={publicLinks?.Inventory}>
-            <i className="bi bi-box-arrow-left"></i>
-          </Link>
-        </span>
-        Site Equipment
-      </h2>
+      <div>
+        <h2 className="fw-bold ">
+          <span>
+            <Link className="btn btn-primary me-2" to={publicLinks?.Inventory}>
+              <i className="bi bi-box-arrow-left"></i>
+            </Link>
+          </span>
+          Site Equipment
+        </h2>
+        <hr />
+      </div>
     </div>
   );
 }
