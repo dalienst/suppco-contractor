a47bf38925af9b3466c0121eb11a8a1593888539
dalienst <oduordalienst@gmail.com>
2024-04-22 17:34:58 +0300

    display builders plant

--- src/pages/BuildersPlant.jsx
+++ src/pages/BuildersPlant.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { publicLinks } from "../constants/links";
+import { Link } from "react-router-dom";
+import { buildersPlant } from "../data/inventory/buidersPlant";
 
 function BuildersPlant() {
   return (
-    <div>BuildersPlant</div>
-  )
+    <>
+      <div className="container">
+        <div>
+          <h2 className="fw-bold ">
+            <span>
+              <Link
+                className="btn btn-primary me-2"
+                to={publicLinks?.Inventory}
+              >
+                <i className="bi bi-box-arrow-left"></i>
+              </Link>
+            </span>
+            Builder's Plant
+          </h2>
+          <hr />
+        </div>
+
+        <section>
+          <div className="table-responsive">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Type</th>
+                  <th>Manufacturer</th>
+                  <th>Model</th>
+                  <th>Year</th>
+                  <th>Weight</th>
+                  <th>Lifting Height</th>
+                  <th>Horsepower</th>
+                  <th>Lease Type</th>
+                  <th>Rate</th>
+                  <th>Supplier</th>
+                </tr>
+              </thead>
+              <tbody>
+                {buildersPlant?.map((item) => (
+                  <>
+                    <tr key={item.id}>
+                      <td>{item.title}</td>
+                      <td>{item.manufacturer}</td>
+                      <td>{item.model}</td>
+                      <td>{item.year}</td>
+                      <td>{item.weight}</td>
+                      <td>{item.liftingHeight}</td>
+                      <td>{item.horsepower}</td>
+                      <td>{item.leaseType}</td>
+                      <td>{item.ratePerHour}</td>
+                      <td>{item.supplier?.name}</td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default BuildersPlant
+export default BuildersPlant;
