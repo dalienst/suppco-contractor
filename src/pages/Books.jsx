c194c017e68fdc4f58719ea84a4ef5ea493acbdb
dalienst <oduordalienst@gmail.com>
2024-04-25 12:45:15 +0300

    add invoice modal

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import PageBanner from "../components/pages/PageBanner";
 import { dummyConstructionData } from "../data/OrdersData";
+import { Link } from "react-router-dom";
+import InvoiceModal from "../layouts/books/InvoiceModal";
 
 function Books() {
+  const [selectedOrderId, setSelectedOrderId] = useState(null);
   const pendingOrders = dummyConstructionData.filter(
     (order) => order.status === "Pending"
   );
@@ -34,7 +37,41 @@ function Books() {
     <>
       <div className="container py-3">
         <PageBanner name="Books of Accounting" />
-        <section className="mt-2 py-2">
+        <section>
+          <h5 className="fw-bold text-secondary text-uppercase">
+            transaction history
+          </h5>
+          <div className="row">
+            <div className="col-md-4 col-sm-12 mb-2">
+              <div className="card">
+                <h6 className="card-header">Money In</h6>
+                <div className="card-body">
+                  <p className="card-text fw-bold">Ksh 0</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-12 mb-2">
+              <div className="card">
+                <h6 className="card-header">Money Out</h6>
+                <div className="card-body">
+                  <p className="card-text fw-bold">
+                    Ksh {completedOrdersTotalPrice}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-12 mb-2">
+              <div className="card">
+                <h6 className="card-header">VAT Paid</h6>
+                <div className="card-body">
+                  <p className="card-text fw-bold">Ksh 0</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <hr />
+        <section className="py-2">
           <h5 className="fw-bold text-secondary text-uppercase">
             Orders Summary
           </h5>
@@ -83,38 +120,56 @@ function Books() {
         <hr />
         <section>
           <h5 className="fw-bold text-secondary text-uppercase">
-            transaction history
+            Recent Orders
           </h5>
-          <div className="row">
-            <div className="col-md-4 col-sm-12 mb-2">
-              <div className="card">
-                <h6 className="card-header">Money In</h6>
-                <div className="card-body">
-                  <p className="card-text fw-bold">Ksh 0</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 col-sm-12 mb-2">
-              <div className="card">
-                <h6 className="card-header">Money Out</h6>
-                <div className="card-body">
-                  <p className="card-text fw-bold">
-                    Ksh {completedOrdersTotalPrice}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 col-sm-12 mb-2">
-              <div className="card">
-                <h6 className="card-header">VAT Paid</h6>
-                <div className="card-body">
-                  <p className="card-text fw-bold">Ksh 0</p>
-                </div>
-              </div>
-            </div>
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Product</th>
+                  <th>Supplier Name</th>
+                  <th>Supplier Company</th>
+                  <th>Date</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {activeOrders.map((item) => (
+                  <>
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.product}</td>
+                      <td>{item.supplierName}</td>
+                      <td>{item.supplierCompany}</td>
+                      <td>{item.date}</td>
+                      <td>
+                        <Link
+                          className="btn btn-sm btn-outline-primary"
+                          onClick={() => {
+                            setSelectedOrderId(item.id);
+                          }}
+                        >
+                          Invoice
+                        </Link>
+                      </td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
           </div>
         </section>
       </div>
+
+      {selectedOrderId && (
+        <InvoiceModal
+          order={activeOrders.find((item) => item.id === selectedOrderId)}
+          onClose={() => {
+            setSelectedOrderId(null);
+          }}
+        />
+      )}
     </>
   );
 }
